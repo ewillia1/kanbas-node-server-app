10b6bb5152f8970e2904c54bcf795968ebcae321
ewillia1 <71227049+ewillia1@users.noreply.github.com>
2024-03-11 11:47:57 -0400

    2.7 Configuring Node.js to use ES6 and 2.8 Creating HTTP Routes.

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
-const express = require('express')                              // Equivalent to import.
+// const express = require('express')                              // Equivalent to import.
+import express from 'express';                                  // Now we can use import syntax instead of require.
+import Hello from "./Hello.js"
+
 const app = express()                                           // Create new express instance.
-// app.get('/hello', (req, res) => {res.send('Hello World!')})
-app.get('/hello', (req, res) => {res.send('Life is good!')})
-app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
+Hello(app)
 app.listen(4000)                                                // Listen to http://localhost:4000
--- Hello.js
+++ Hello.js
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+// console.log("Hello World!");
+// app.get('/hello', (req, res) => {res.send('Hello World!')})
+// app.get('/hello', (req, res) => {res.send('Life is good!')})
+// app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
+
+// Function accepts app reference to express module to create routes here. We could have used the new arrow function syntax instead.
+export default function Hello(app) {
+    app.get('/hello', (req, res) => {
+        res.send('Life is good!')
+    })
+    app.get('/', (req, res) => {
+        res.send('Welcome to Full Stack Development!')
+    })
+}  
