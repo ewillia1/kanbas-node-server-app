043182144f76c107f4d34f7d623dd48f6a324fc9
ewillia1 <71227049+ewillia1@users.noreply.github.com>
2024-03-11 12:32:05 -0400

    3.1.1 Path Parameters.

--- Lab5.js
+++ Lab5.js
@@ -2,5 +2,17 @@ const Lab5 = (app) => {                             // Accept app reference to e
     app.get("/a5/welcome", (req, res) => {          // Create route to welcome users to assignment 5.
         res.send("Welcome to Assignment 5");        // Here we are using the arrow function syntax.
     });
+
+    app.get("/a5/add/:a/:b", (req, res) => {        // Route expexts 2 path parameters after /a5/add
+        const { a, b } = req.params;                // Retrieve path parameters as strings.
+        const sum = parseInt(a) + parseInt(b);      // Parse as integers and adds.
+        res.send(sum.toString());                   // Sum as string sent back as response.
+    });                                             // Don't send integers since they can be interpreted as status.
+
+    app.get("/a5/subtract/:a/:b", (req, res) => {   // Route expects 2 path parameters after /a5/subtract
+        const { a, b } = req.params;                // Retrieve path parameters as strings.
+        const sum = parseInt(a) - parseInt(b);      // Parse as integers and subtracts.
+        res.send(sum.toString());                   // Subtraction as string sent back as response.
+    });                                             // Response is converted to string otherwise browser would interpret integer response as a status code.
 };
 export default Lab5;
