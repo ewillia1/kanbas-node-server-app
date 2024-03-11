507141d955300f4ca76ec26d6b6df3a4de5e3dd9
ewillia1 <71227049+ewillia1@users.noreply.github.com>
2024-03-11 14:16:21 -0400

    3.1.2 Query Parameters. 3.1.3. On Your Own.

--- Lab5.js
+++ Lab5.js
@@ -14,5 +14,39 @@ const Lab5 = (app) => {                             // Accept app reference to e
         const sum = parseInt(a) - parseInt(b);      // Parse as integers and subtracts.
         res.send(sum.toString());                   // Subtraction as string sent back as response.
     });                                             // Response is converted to string otherwise browser would interpret integer response as a status code.
+
+    app.get("/a5/multiply/:a/:b", (req, res) => {
+        const { a, b } = req.params;
+        const sum = parseInt(a) * parseInt(b);
+        res.send(sum.toString());
+    });
+
+    app.get("/a5/divide/:a/:b", (req, res) => {
+        const { a, b } = req.params;
+        const sum = parseInt(a) / parseInt(b);
+        res.send(sum.toString());
+    });
+
+    app.get("/a5/calculator", (req, res) => {       // e.g., a5/calculator?a=5&b=2&operation=add
+        const { a, b, operation } = req.query;      // Retrieve a, b, and operation parameters in query.
+        let result = 0;
+        switch (operation) {
+            case "add":
+                result = parseInt(a) + parseInt(b);     // Parse as integers since parameters are strings.
+                break;
+            case "subtract":
+                result = parseInt(a) - parseInt(b);
+                break;
+            case "multiply":
+                result = parseInt(a) * parseInt(b);
+                break;
+            case "divide":
+                result = parseInt(a) / parseInt(b);
+                break;
+            default:
+                result = "Invalid operation";
+        }
+        res.send(result.toString());                // Convert to string otherwise browser interprets as a status code.
+    });
 };
 export default Lab5;
