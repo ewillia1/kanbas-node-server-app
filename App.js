1e244b633a16f90d4de665c639ed3d62d3e4bfda
ewillia1 <71227049+ewillia1@users.noreply.github.com>
2024-03-11 21:16:26 -0400

    3.4.2 Configuring Cross Origin Request Sharing (CORS).

--- App.js
+++ App.js
@@ -2,8 +2,10 @@
 import express from 'express';                                  // Now we can use import syntax instead of require.
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
+import cors from "cors";                                        // Import cors library.
 
-const app = express();                                           // Create new express instance.
+const app = express();                                          // Create new express instance.
+app.use(cors());                                                // Make sure cors is used right after creating the app express instance.
 Lab5(app);
 Hello(app);
 app.listen(4000);                                               // Listen to http://localhost:4000
