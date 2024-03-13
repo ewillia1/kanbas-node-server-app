902f622401dd0d6c0738345603a01d8acb802619
ewillia1 <71227049+ewillia1@users.noreply.github.com>
2024-03-13 15:45:07 -0400

    4.2.2 Creating New Courses.

--- Kanbas/courses/routes.js
+++ Kanbas/courses/routes.js
@@ -1,6 +1,16 @@
 import Database from "../Database/index.js";
 
 function CourseRoutes(app) {
+    // Route that creates a new course and adds it to the Database.
+    // New course is passed into the HTTP body from the client and is appended to the end of the courses array in the Dashboard.
+    // New course is given a new unique identifier and sent back to the client in the response.
+    app.post("/api/courses", (req, res) => {
+        const course = { ...req.body, _id: new Date().getTime().toString() };
+        Database.courses.push(course);
+        res.send(course);
+    });
+
+    // Route that makes courses available at http://localhost:4000/api/courses.
     app.get("/api/courses", (req, res) => {
         const courses = Database.courses;
         res.send(courses);
