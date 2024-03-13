50560e0cd1957782d4f42152d5b8337cea09c12c
ewillia1 <71227049+ewillia1@users.noreply.github.com>
2024-03-13 15:36:41 -0400

    4.2.1 Retrieving Courses.

--- App.js
+++ App.js
@@ -2,11 +2,13 @@
 import express from 'express';                                  // Now we can use import syntax instead of require.
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
+import CourseRoutes from "./Kanbas/courses/routes.js";
 import cors from "cors";                                        // Import cors library.
 
 const app = express();                                          // Create new express instance.
 app.use(cors());                                                // Make sure cors is used right after creating the app express instance.
 app.use(express.json());                                        // Make sure this statement occurs AFTER setting up CORS.
+CourseRoutes(app);
 Lab5(app);
 Hello(app);
 app.listen(4000);                                               // Listen to http://localhost:4000
