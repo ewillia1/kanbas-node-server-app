ece09b67ebf0f045fa7bccb48f83ff45274ff8bd
ewillia1 <71227049+ewillia1@users.noreply.github.com>
2024-03-13 21:37:56 -0400

    4.3.3 Deleting a Module.

--- Kanbas/modules/routes.js
+++ Kanbas/modules/routes.js
@@ -1,6 +1,19 @@
 import db from "../Database/index.js";
 
 function ModuleRoutes(app) {
+    // Route that handles an HTTP DELETE method with the module's ID embedded in the URL.
+    // mid request parameter is used to filter out the module from the modules array in the database.
+    // Respond with a 200 status signifying success.
+    // Return a 404 if the module is not found.
+    app.delete("/api/modules/:mid", (req, res) => {
+        const { mid } = req.params;
+        db.modules = db.modules.filter((m) => m._id !== mid);
+        if (!db.modules) {
+            res.status(404).json({ message: `Unable to delete Module with ID ${id}` });
+        }
+        res.sendStatus(200);
+    });
+    
     // Route that receives a new module posted from the React.js client application embedded in the Request body.
     // Create a new module object setting its source property to the cid path parameter and push it to the db.modules array.
     // Reply to client with the newly created module.
