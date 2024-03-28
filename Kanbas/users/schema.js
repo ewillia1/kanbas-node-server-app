ecf81e8f2de63bf2772ee8f4ab4d49f63d65539c
ewillia1 <71227049+ewillia1@users.noreply.github.com>
2024-03-28 15:14:32 -0400

    Created Kanbas/users/schema.js.
    3.2. Implementing Mongoose schemas and models.

---
+++ Kanbas/users/schema.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({            // Creat the schema.
+    username: {                                     // String field that is required and unique.
+        type: String, 
+        required: true, 
+        unique: true 
+    },
+    password: {                                     // String field that is required but not unique.
+        type: String, 
+        required: true 
+    },
+    firstName: String,                              // String fields
+    email: String,                                  // with no additional
+    lastName: String,                               // configurations.
+    dob: Date,                                      // Date field with no configurations.
+    role: {
+        type: String,                                   // String field
+        enum: ["STUDENT", "FACULTY", "ADMIN", "USER"],  // allowed string values
+        default: "USER"                                 // default value if not provided.
+    },
+},{ collection: "users" });                         // Store data in "users" collection.
+export default userSchema;
