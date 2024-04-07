b592086be2ee1cd7ae27f4ebcd22b0acc3b624b2
ewillia1 <71227049+ewillia1@users.noreply.github.com>
2024-04-06 21:53:28 -0400

    Working on getting courses and assignments working with MongoDB.
    Created empty files.

--- Kanbas/courses/model.js
+++ Kanbas/courses/model.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import courseSchema from "./schema.js";
 
-// Once you give a model a name (ex. Course), it is unique.
-const courseModel = mongoose.model("Course", courseSchema);
+// Once you give a model a name (ex. Courses), it is unique.
+const courseModel = mongoose.model("Courses", courseSchema);
 export default courseModel;
