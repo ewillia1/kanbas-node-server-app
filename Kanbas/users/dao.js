a24ceba7c8be35999347a9623b774bea0b428179
ewillia1 <71227049+ewillia1@users.noreply.github.com>
2024-04-07 11:34:01 -0400

    Updated comments and location of code in /users/dao.js and users/model.js.

--- Kanbas/users/dao.js
+++ Kanbas/users/dao.js
@@ -12,14 +12,14 @@ export const findAllUsers = () => userModel.find();
 
 export const findUserById = (userId) => userModel.findById(userId);
 
+export const updateUser = (userId, user) =>  userModel.updateOne({ _id: userId }, { $set: user });
+
+export const deleteUser = (userId) => userModel.deleteOne({ _id: userId });
+
 export const findUserByEmail = (email) => userModel.findOne({email});
 
 export const findUserByUsername = (username) =>  userModel.findOne({ username: username });
 
 export const findUserByCredentials = (username, password) =>  userModel.findOne({ username: username, password: password });
-
-export const updateUser = (userId, user) =>  userModel.updateOne({ _id: userId }, { $set: user });
-
-export const deleteUser = (userId) => userModel.deleteOne({ _id: userId });
 
 export const findUsersByRole = (role) => userModel.find({ role: role });
--- Kanbas/users/model.js
+++ Kanbas/users/model.js
@@ -2,4 +2,4 @@ import mongoose from "mongoose";
 import schema from "./schema.js";
 
 const userModel = mongoose.model("Users", schema);           // Create mongoose model from the schema.
-export default userModel;                                   // Export so it can be used elsewhere.
+export default userModel;                                    // Export so it can be used elsewhere.
