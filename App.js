e626241618df469ff98e2ac05495e2b6b8341a65
ewillia1 <71227049+ewillia1@users.noreply.github.com>
2024-04-09 09:47:14 -0400

    Updated App.js.

--- App.js
+++ App.js
@@ -5,10 +5,12 @@ import CourseRoutes from "./Kanbas/courses/routes.js";
 import cors from "cors";
 import ModuleRoutes from './Kanbas/modules/routes.js';
 import AssignmentRoutes from './Kanbas/assignments/routes.js';
+import EnrollmentRoutes from './Kanbas/enrollments/routes.js';
+import GradeRoutes from './Kanbas/grades/routes.js';
 import UserRoutes from './Kanbas/users/routes.js';
 import LikesRoutes from "./Napster/likes/routes.js";
 import mongoose from 'mongoose';
-import session from 'express-session'; // Import express-session for session management.
+import session from 'express-session';  // Import express-session for session management.
 import dotenv from 'dotenv';            // Import to read .env file.
 
 dotenv.config(); 
@@ -21,7 +23,7 @@ const app = express();
 app.use(
     cors({
         origin: process.env.FRONTEND_URL,    // Restrict cross origin resource shareing to the react application.
-        credentials: true,                  // Suport cookies.
+        credentials: true,                   // Support cookies.
     })
 );
  
@@ -30,7 +32,7 @@ app.use(express.json());
 // Initialize session middleware. Configure server session AFTER cors.
 // sessionOptions = default session options.
 const sessionOptions = {
-    secret: process.env.SESSION_SECRET, // Add a secret key for session encryption.
+    secret: process.env.SESSION_SECRET,      // Add a secret key for session encryption.
     resave: false,
     saveUninitialized: false
 };
@@ -48,6 +50,8 @@ app.use(session(sessionOptions));
 ModuleRoutes(app);
 CourseRoutes(app);
 AssignmentRoutes(app);
+EnrollmentRoutes(app);
+GradeRoutes(app);
 UserRoutes(app);
 LikesRoutes(app);
 Lab5(app);
