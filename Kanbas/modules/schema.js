253aab09a6e5ede22b55ac5fcd4a9444757c8eb2
ewillia1 <71227049+ewillia1@users.noreply.github.com>
2024-04-14 22:33:36 -0400

    Updated /modules/schema.js to include lessons.

--- Kanbas/modules/schema.js
+++ Kanbas/modules/schema.js
@@ -1,9 +1,20 @@
 import mongoose from "mongoose";
 
+const lessonSchema = new mongoose.Schema({
+    _id: String,
+    name: String,
+    description: String,
+    module: String
+});
+
 const moduleSchema = new mongoose.Schema({
     id: String,
     name: String,
     description: String,
-    course: String
+    course: String,
+    lessons: {
+        type: [lessonSchema],
+        required: false
+    }
 }, { collection: "modules" });
 export default moduleSchema;
